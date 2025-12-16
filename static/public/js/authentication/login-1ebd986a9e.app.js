376c9c63fe24b903f723936e7e4fea0eaf0f9f66
kernel64-dll <garciaalangarcia494@gmail.com>
2025-12-16 04:30:18 +0100

    delete unused stuff

--- static/public/js/authentication/login-1ebd986a9e.app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-require(["angularDependency", "redesign/globalModules", "authentication/login-form"], function (angular) {
-    angular.bootstrap(document, ['globalModules']);
-});
-
-//# sourceMappingURL=login.app.js.map
