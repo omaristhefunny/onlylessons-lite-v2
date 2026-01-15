86e6fe1d06a552b076d8d7907f07d454bcd122fe
kernel64-dll <garciaalangarcia494@gmail.com>
2026-01-15 05:32:39 +0100

    delete unused files

--- static/public/js/sur.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const cursor = document.querySelector('.custom-cursor');
-    cursor.style.display = 'block';
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-});
