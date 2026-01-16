ff5bbe41ed2fbc912d28500ed0bc277bd0984b70
Tilly <corbinschristensen@gmail.com>
2026-01-15 19:06:09 -0700

    AuthSystem FINAL

--- static/public/js/chatsystem.js
+++ static/public/js/chatsystem.js
@@ -234,7 +234,10 @@ DOM.form.addEventListener("submit", (event) => {
 }
   const value = DOM.input.value.trim();
   if (!value) return;
-
+   if(value.match(/(黑鬼|ass|cum|retard|bitch|shit|cunt|cock|dick|fuck|shit|nigger|nigga|pussy|nazi|whore|faggot|handjob|penis|cock|pussy|sex|hitler|niger|titties|gay|tit|boob|@ss|c0ck|b!tch|pu\$\$y|por|nigas|pp|incest|p0r|rape|r@pe|slut|threesum|foursum|twosum|shiz|slut|p0r|nigg)/gi)){
+    alert('cmon man why you saying that kinda stuff?');
+    return;
+  }
   DOM.input.value = "";
 
   drone.publish({
